2cc61e2e0187ed3f367b1f8d129ece59113dbbba
palakmanocha <palakmanocha99@gmail.com>
2019-07-22 15:12:46 +0530

    Updates

---
+++ precache-manifest.655592fcd25bb2136f0bdc3e87b2ead7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12b9e8c93db89dd18fbce61886c2b6a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "405f0cd0e0265087661a",
+    "url": "/portfolio/static/css/2.86f1f90d.chunk.css"
+  },
+  {
+    "revision": "6e98684d3f2a538ba1e6",
+    "url": "/portfolio/static/css/main.c4dd9138.chunk.css"
+  },
+  {
+    "revision": "405f0cd0e0265087661a",
+    "url": "/portfolio/static/js/2.439226eb.chunk.js"
+  },
+  {
+    "revision": "6e98684d3f2a538ba1e6",
+    "url": "/portfolio/static/js/main.d25d45f2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "dc2bf60ceb6cdf3825242c3c6a5403b8",
+    "url": "/portfolio/static/media/me.dc2bf60c.jpg"
+  }
+]);
